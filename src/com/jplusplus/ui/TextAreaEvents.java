dcdfa6dce03cb7329e6731bdbfaeb503964b23c2
Keeboi <kiboy5@yahoo.com>
2018-02-10 17:06:17 +0800

    Fixed caret position not updating.

--- src/com/jplusplus/ui/TextAreaEvents.java
+++ src/com/jplusplus/ui/TextAreaEvents.java
@@ -17,5 +17,14 @@ public class TextAreaEvents {
                 text.setText(count+ "Lines, "+caretPos+" Caret Position.");
             }
         });
+        area.caretPositionProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                String[] countStr = area.getText().split("\n");
+                int count = countStr.length;
+                int caretPos = area.getCaretPosition();
+                text.setText(count+ "Lines, "+caretPos+" Caret Position.");
+            }
+        });
     }
 }
